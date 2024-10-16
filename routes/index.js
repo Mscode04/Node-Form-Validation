8e21764ded2bbefb1d5cb17a74bba438e46b26a1
Mscode04 <ms.shaheenkp@gmail.com>
2024-10-16 20:26:24 +0530

    done

---
+++ routes/index.js
@@ -0,0 +1,94 @@
+const express = require('express');
+const router = express.Router();
+const { MongoClient } = require('mongodb');
+const path = require('path');
+
+
+router.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname, '../views/login.html')); 
+});
+router.get('/homee', (req, res) => {
+  res.sendFile(path.join(__dirname, '../views/hm.html')); 
+});
+router.get('/er', (req, res) => {
+  res.sendFile(path.join(__dirname, '../views/error.html')); 
+});
+
+
+
+
+router.get('/', function(req, res, next) {
+    res.render('index', { title: 'Express' });
+});
+
+
+// WHEN SUBMIT DATA
+router.post('/up', async function (req, res) {
+  const url = 'mongodb://localhost:27017';
+  const client = new MongoClient(url);
+  
+  try {
+      await client.connect(); 
+      const db = client.db('dex'); 
+      const users = await db.collection('user').find({}).toArray();
+
+      var arremail = []; 
+      for (let i = 0; i < users.length; i++) {
+          arremail.push(users[i].email); 
+      }
+      var char = ['@', '#', ','];
+      if (!/[A-Z]/.test(req.body.pw) || !/[a-z]/.test(req.body.pw) || !char.some(symbol => req.body.pw.includes(symbol))) {
+        return res.render('index', { title: 'Ms Form', errorMessage: 'Password must contain at least one uppercase letter, one lowercase letter, and one symbol (@, #, or ,)!' });
+    }
+    if (req.body.pw !== req.body.cpw) {
+      return res.render('index', { title: 'Ms Form', errorMessage: 'Passwords do not match!' });
+  }
+      
+      if (arremail.includes(req.body.email)) {
+          console.log("Login");
+          return res.redirect('/login'); 
+      } else {
+          // Proceed with registration
+          await db.collection('user').insertOne(req.body);
+          return res.redirect('/homee'); 
+      }
+
+  } catch (err) {
+      console.error('Error fetching users:', err);
+      res.status(500).send('Failed to fetch users');
+  } finally {
+      await client.close(); 
+  }
+});
+
+
+// WHEN SUBMIT DATA
+router.post('/sub', async function (req, res) {
+  const url = 'mongodb://localhost:27017';
+  const client = new MongoClient(url);
+  try {
+      await client.connect(); 
+      const db = client.db('dex'); 
+      const users = await db.collection('user').find({}).toArray();
+
+      var arremail=[] 
+      for(i=0;i<users.length;i++){
+        arremail.push(users[i].email) 
+      }
+      
+      let index = arremail.indexOf(req.body.email)
+      if (req.body.pw==users[index].pw) {
+        return res.redirect('/homee'); 
+      } else {
+        return res.redirect('/login'); 
+      }
+    
+  } catch (err) {
+      console.error('Error fetching users:', err);
+      res.status(500).send('Failed to fetch users');
+  } finally {
+      await client.close(); 
+  }
+
+});
+module.exports = router;
